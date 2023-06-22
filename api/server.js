6ba6e9509cd78b949b120282084622ea6274c035
JosiahSchwahn <schwahn.josiah@gmail.com>
2023-06-22 15:13:38 -0600

    added api

---
+++ api/server.js
@@ -0,0 +1,13 @@
+// api/server.js
+
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res) {
+    res.send("It's working!")
+  });
+  
+
+app.listen(3001, () => {
+  console.log("app listening on port 3000")
+});
